e0999db1b78315fbdb9590a5586f80348b2e1ef4
Anders Hofsten <anders.jakobsen.hofsten@gmail.com>
2024-01-29 07:17:54 +0100

    Added movie search functionality to App component. The searchMovies function fetches data from an API and logs the results. This function is called once on component mount using the useEffect hook.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useEffect } from 'react';
+// API key for omdb: 8e16f5a1
+// delete this when production!
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const API_URL = 'http://www.omdbapi.com/?apikey=8e16f5a1';
+
+const App = () => {
+
+	const searchMovies = async (title) => {
+		const response = await fetch(`${API_URL}&s=${title}`);
+		const data = await response.json();
+		console.log(data.Search);
+	};
+	// render once, since the second argument is an empty array
+	useEffect(() => {
+		searchMovies('Matrix');
+	}, []);
+
+
+	return (
+		<div>Find your movie</div>
+	);
+};
 
 export default App;
+
